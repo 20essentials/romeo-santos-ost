9c693399419550ddaec22f388fcc4a20fcecdfd2
DominicDev <s0l0estudi4r@gmail.com>
2026-03-13 11:05:26 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'ROMEO SANTOS';
+export let nameSongs = [
+	"PROPUESTA INDECENTE", /*1*/
+	"ERES MÍA", /*2*/
+	"IMITADORA", /*3*/
+	"CANCIONCITAS DE AMOR", /*4*/
+	"HILITO", /*5*/
+	"NECIO", /*6*/
+	"ODIO ", /*7*/
+	"SOBREDOSIS", /*8*/
+	"MI SANTA", /*9*/
+	"LA DIABLA", /*10*/
+	"SOLO CONMIGO", /*11*/
+	"BEBO", /*12*/
+	"BOOMERANG", /*13*/
+	"15,500 NOCHES", /*14*/
+	"SUS HUELLAS", /*15*/
+	"EL PAÑUELO", /*16*/
+	"YO TAMBIÉN", /*17*/
+	"FUI A JAMAICA", /*18*/
+	"LLÉVAME CONTIGO", /*19*/
+	"BELLA Y SENSUAL", /*20*/
+]
